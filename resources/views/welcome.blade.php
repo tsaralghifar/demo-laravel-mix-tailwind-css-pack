7229507d75dee201e572e41bdd2704cb8871a9b6
tsaralghifar <alghifaridev@gmail.com>
2021-02-18 14:33:33 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        
+    </head>
+    <body class="antialiased">
+        <div class="relative bg-white" style="width: 1440px; height: 1024px;">
+            <p class="absolute text-6xl font-medium text-gray-700" style="left: 150px; top: 154px;">Hello! Saya <br/>Tsar Alghifari,<br/>Seorang Web <br/>Programmer</p>
+            <p class="absolute text-2xl font-medium text-green-700" style="left: 845px; top: 61px;">Portfolio</p>
+            <p class="absolute text-2xl font-medium text-green-700" style="left: 1028px; top: 61px;">Certificate</p>
+            <div class="w-20 h-10 absolute" style="left: 1198px; top: 59px;">
+                <div class="flex items-center justify-start flex-1 h-full py-2 pl-2 pr-12 bg-blue-400 rounded-full">
+                    <div class="relative" style="width: 27px; height: 25px;">
+                        <div class="inline-flex flex-col space-y-44 items-end justify-start w-3.5 pb-2 absolute left-0 top-0">
+                            <div class="rotate-180 w-1 h-0.5 bg-yellow-300 rounded-full"></div>
+                            <div class="-rotate-45 w-0.5 h-1/3 bg-yellow-300 rounded-full"></div>
+                            <div class="w-0.5 h-1/3 bg-yellow-300 rounded-full"></div>
+                            <div class="w-1/3 h-0.5 bg-yellow-300 rounded-full"></div>
+                        </div>
+                        <div class="inline-flex flex-col space-y-44 items-end justify-start w-3.5 absolute right-0 bottom-0">
+                            <div class="rotate-180 w-0.5 h-1/5 bg-yellow-300 rounded-full"></div>
+                            <div class="-rotate-45 w-1 h-0.5 bg-yellow-300 rounded-full"></div>
+                            <div class="w-1/3 h-0.5 bg-yellow-300 rounded-full"></div>
+                            <div class="w-0.5 h-1/5 bg-yellow-300 rounded-full"></div>
+                        </div>
+                        <div class="inline-flex items-center justify-center w-3 h-3 pl-1.5 absolute m-auto inset-0 bg-yellow-300 rounded-full">
+                            <div class="flex-1 h-full bg-yellow-300 rounded-full"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="inline-flex flex-col space-y-5 items-center justify-end absolute" style="width: 661px; height: 232px; left: 150px; top: 556px;">
+                <p class="text-lg font-light leading-loose text-gray-700 text-opacity-75" style="width: 661px;">Seorang Fullstack Web Developer dan UI Designer, membuat website dan design yang  kompleks dan minimalis sesuai kebutuhan user adalah tugas saya. Menghabiskan waktu berjam-jam untuk eksplorasi design dan coding. Kalian dapat melihat karya saya di Github dan Dribble.</p>
+                <p class="text-lg font-light leading-loose text-gray-700 text-opacity-75" style="width: 661px;">Saya aktif di Twitter lebih fokus pembahasan web development, design dan meme, saya sering membaca thread tentang tips design dan coding.</p>
+            </div>
+            <div class="inline-flex items-center justify-center w-10 h-10 px-0.5 pt-0.5 pb-1 absolute" style="left: 240px; top: 828px;">
+                <div class="flex-1 h-full bg-gray-700 rounded-lg"></div>
+            </div>
+            <div class="inline-flex items-center justify-center w-10 h-10 p-0.5 absolute" style="left: 330px; top: 827px;">
+                <div class="flex-1 h-full bg-gray-700 rounded-lg"></div>
+            </div>
+            <div class="inline-flex items-center justify-center w-10 h-10 pt-0.5 pb-1 absolute" style="left: 150px; top: 827px;">
+                <div class="flex-1 h-full bg-gray-700 rounded-lg"></div>
+            </div>
+            <div class="relative absolute" style="width: 519.06px; height: 532.33px; left: 846px; top: 272px;">
+                <div class="relative absolute left-0 top-0" style="width: 519.06px; height: 532.09px;">
+                    <img src="avatar.png" alt="">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
